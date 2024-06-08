26a6dc9fce9a410c8f0d872980b2f3374e9d5da2
SergBo77 <seman77@mail.ru>
2024-06-08 18:36:49 +0300

    add HW-Analis

--- HW_3_Analis.py
+++ HW_3_Analis.py
@@ -10,6 +10,14 @@ df = pd.DataFrame({'name': names, 'subject': random_subjects, 'grade': grades})
 
 print(df.head())
 
-print(f"Средняя оценка - {df.groupby('subject')['grade'].mean()}")
+print(f'\n Средняя оценка по предмету- {df.groupby('subject')['grade'].mean()}')
+print(f'\n Медианная оценка по предмету- {df.groupby('subject')['grade'].median()}')
+print(f'\n Стандартное отклонение по оценкам- {df['grade'].std()}')
 
+print('\nСрез по математике:')
+math_grades = df[df['subject'] == 'math']['grade']
+Q1_math = math_grades.quantile(0.25)
+Q3_math = math_grades.quantile(0.75)
+IQR = Q3_math- Q1_math
 
+print(f'\nПервый квартиль = {Q1_math}, третий квартиль = {Q3_math}, межквартильный размах = {IQR} ')
